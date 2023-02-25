21e4b596704445570d786061ad725cb462dccbdc
William Home <wilsai1023@gmail.com>
2023-02-25 09:05:46 -0500

    cors added

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import { createRoles } from "./libs/initialSetUp";
 import productsRoutes from "./routes/product.routes";
 import authRoutes from "./routes/auth.routes"
 import userRoutes from "./routes/user.routes";
+import cors from "cors";
 
 const app = express();
 createRoles();
 app.use(morgan("dev"));
 app.use(express.json())
+app.use(cors());
 
 app.use("/api/products", productsRoutes)
 app.use("/api/auth", authRoutes)
