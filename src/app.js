3b1dac8f5fc2e625b19282cbe79b95bb6b89cb40
William Home <wilsai1023@gmail.com>
2023-03-08 07:33:43 -0500

    upload carpet

--- src/app.js
+++ src/app.js
@@ -7,16 +7,21 @@ import userRoutes from "./routes/user.routes";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import { errorHandler } from "./middlewares/autorizator";
+import fs from "fs-extra";
 
 const app = express();
 createRoles();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
+if (!fs.existsSync('upload')) {
+  fs.mkdirSync('upload');
+}
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: "./upload"
 }));
+
 app.use("/api/ads", adsRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
