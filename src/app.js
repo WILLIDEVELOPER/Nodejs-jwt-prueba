0c2061e12be5d2c9f23ee7ad1a2fce8a9db00b8f
William Home <wilsai1023@gmail.com>
2023-03-07 20:48:23 -0500

    arreglos

--- src/app.js
+++ src/app.js
@@ -2,23 +2,24 @@ import express from "express";
 import morgan from "morgan";
 import { createRoles } from "./libs/initialSetUp";
 import adsRoutes from "./routes/ad.routes";
-import authRoutes from "./routes/auth.routes"
+import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
 import cors from "cors";
-import configDinary  from "./libs/configDinary";
+import configDinary from "./libs/configDinary";
 import { errorHandler } from "./middlewares/autorizator";
 
 const app = express();
 createRoles();
-configDinary()
+configDinary.uploader.upload("imagen.jpg", (error, result) => {
+  console.log(result, error);
+});
 app.use(morgan("dev"));
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-app.use("/api/ads", adsRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/ads", adsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use(errorHandler);
-
 
 export default app;
--- src/libs/configDinary.js
+++ src/libs/configDinary.js
@@ -1,7 +1,5 @@
 import { v2 } from "cloudinary";
 
-v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+v2.config({ cloud_name: "dstwpmmob", api_key: "385879314331286", api_secret: "0K_aCx1f9XeDzdFyp_aabKKZhU4"});
+
+export default v2;
